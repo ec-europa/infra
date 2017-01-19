d257bb7b6d0478a58be531d2a2b5f254b97b7e14
Gregory Boddin <gregory@siwhine.net>
2017-01-19 20:32:43 +0100

    Debugging

--- docker/images/fpfis/php56/run.sh
+++ docker/images/fpfis/php56/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mount
+export
+ls / -lha
 source /etc/httpd/envvars
 [ ! -z ${DOCROOT} ] && rm /var/www/html -Rf && ln -s "$DOCROOT" /var/www/html
 /usr/sbin/httpd -d /usr/lib64/httpd -f /etc/httpd/conf/httpd.conf -DFOREGROUND
