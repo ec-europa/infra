61858a87dc2894f2db3e69ae9c2e43015e332b98
Gregory Boddin <gregory@siwhine.net>
2017-01-19 21:57:29 +0100

    Making httpd server smarter

--- docker/images/fpfis/php56/run.sh
+++ docker/images/fpfis/php56/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-mount
-export
-ls -lha
+set -e
+if [ ! -z "${DOCROOT}" ]; then
+   rm /var/www/html -Rf
+   # This allows for a future not-yet-created build folder to be created
+   [ -d "${DOCROOT}" ] || mkdir "${DOCROOT}" 
+   ln -s "$DOCROOT" /var/www/html
+fi
 source /etc/httpd/envvars
-[ ! -z ${DOCROOT} ] && rm /var/www/html -Rf && ln -s "$DOCROOT" /var/www/html
 /usr/sbin/httpd -d /usr/lib64/httpd -f /etc/httpd/conf/httpd.conf -DFOREGROUND
